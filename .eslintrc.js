1cf9b2b41505df5e549e8444d9f308bfe957e914
florensg <floo.g1096@gmail.com>
2024-06-09 17:13:05 -0300

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-      'no-undef': 'error',
-    },
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true, // Esto habilita las reglas de ESLint para Jest
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-undef': 'error',
+  },
+};
